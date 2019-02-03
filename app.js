2bbfe81e8dc4f6f824ec71968de14151d99087a8
cwinters8 <clwinters8@gmail.com>
2019-02-03 14:18:12 -0800

    a whole bunch of changes because I failed to commit often

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const data = require('./data.json');
+const path = require('path');
+
+// create the app
+const app = express();
+
+/******************
+ *** Middleware ***
+ ******************/
+// set the view engine to pug
+app.set('view engine', 'pug');
+
+// route for static files
+app.use('/static', express.static('public'));
+
+/******************
+ ***** Routes *****
+ ******************/
+// home page
+app.get('/', (req, res) => {
+    res.render('index', data.projects);
+});
+
+// about page
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// projects
+data.projects.forEach((project, index) => {
+    app.get(`/project/${project.id}`, (req, res) => {
+        res.render('project', data.projects[index]);
+    });
+});
+// in case a user navigates directly to /project
+app.get('/project', (req, res) => {
+    res.redirect('/project/0');
+});
+
+/******************
+ * Error Handling *
+ ******************/
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    console.log(`${err.status}: Page not found`);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.render('error', err);
+});
+
+// start the app
+app.listen(3000, () => console.log('Listening on port 3000'));
