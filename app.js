815aa71be54f32f5c4234a9666ddcc3d6cea282b
cwinters8 <clwinters8@gmail.com>
2019-02-05 00:00:34 -0800

    updated port for Heroku

--- app.js
+++ app.js
@@ -54,4 +54,5 @@ app.use((err, req, res, next) => {
 });
 
 // start the app
-app.listen(3000, () => console.log('Listening on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening on port 3000'));
